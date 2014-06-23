3f53c9c6b4808bf9f46241fe2d8d5019836a8f92
Rico Sta. Cruz <hi@ricostacruz.com>
2014-06-23 11:06:09 +0800

    Remove extraneous jquery tests.

--- test/setup.js
+++ test/setup.js
@@ -1,26 +1,20 @@
-// Deps
 global.chai = require('chai');
 global.assert = chai.assert;
 chai.should();
 
 var fs = require('fs');
-var multisuite = require('./support/multisuite');
 
 var scripts = {
-  'jq-1.7':  fs.readFileSync('vendor/jquery-1.7.js'),
-  'jq-1.8':  fs.readFileSync('vendor/jquery-1.8.js'),
-  'jq-1.9':  fs.readFileSync('vendor/jquery-1.9.js'),
-  'jq-1.10': fs.readFileSync('vendor/jquery-1.10.js'),
   'jq-2.0':  fs.readFileSync('vendor/jquery-2.0.js'),
   'nprogress': fs.readFileSync('nprogress.js')
 };
 
-function myEnv(jq) {
+function myEnv() {
   var jsdom = require('jsdom');
   return function(done) {
     jsdom.env({
       html: '<!doctype html><html><head></head><body></body></html>',
-      src: [ scripts[jq], scripts.nprogress ],
+      src: [ scripts['jq-2.0'], scripts.nprogress ],
       done: function(errors, window) {
         window.console = console;
         global.window  = window;
@@ -33,9 +27,5 @@ function myEnv(jq) {
   };
 }
 
-if (process.env.fast) {
-  before(myEnv('jq-1.10'));
-  global.testSuite = describe;
-} else {
-  global.testSuite = multisuite(['jq-1.8', 'jq-1.9', 'jq-1.10', 'jq-2.0'], myEnv);
-}
+before(myEnv());
+global.testSuite = describe;
