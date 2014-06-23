5d0611444e05a7922658db68df54ca9975d8802d
Rico Sta. Cruz <hi@ricostacruz.com>
2014-06-23 11:13:09 +0800

    Revert removing jQuery from tests.

--- test/setup.js
+++ test/setup.js
@@ -5,6 +5,7 @@ chai.should();
 var fs = require('fs');
 
 var scripts = {
+  'jq-2.0':  fs.readFileSync('vendor/jquery-2.0.js'),
   'nprogress': fs.readFileSync('nprogress.js')
 };
 
@@ -13,7 +14,7 @@ function myEnv() {
   return function(done) {
     jsdom.env({
       html: '<!doctype html><html><head></head><body></body></html>',
-      src: [ scripts.nprogress ],
+      src: [ scripts['jq-2.0'], scripts.nprogress ],
       done: function(errors, window) {
         window.console = console;
         global.window  = window;
